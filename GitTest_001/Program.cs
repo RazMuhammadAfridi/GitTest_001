bd37c781139f8c0a2db9567baac438311a20f7da
Raz Muhammad <Raz_Afridi@yahoo.com>
2022-11-07 15:19:07 +0500

    3

--- GitTest_001/Program.cs
+++ GitTest_001/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.    Comment_01 , comment_02
+// Add services to the container.    Comment_01 , comment_02 ,03
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
